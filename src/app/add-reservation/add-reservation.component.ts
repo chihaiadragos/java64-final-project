8f1bd1dc6876850fbfb5cec56eb0b2c15ae463e8
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-05-21 21:26:49 +0300

    reservation list, add reservation

---
+++ src/app/add-reservation/add-reservation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-reservation',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-reservation.component.html',
+  styleUrl: './add-reservation.component.css'
+})
+export class AddReservationComponent {
+
+}
