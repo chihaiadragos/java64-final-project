39d7b098cefd9827a6b459ee901d12d92ba2918e
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-05-26 23:31:15 +0300

    employee service

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private url = "http://localhost:8080/employees";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll() {
+    return this.http.get<any>(this.url);
+  }
 }
