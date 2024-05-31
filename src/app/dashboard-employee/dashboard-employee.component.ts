a25fd9b89601b411e5c645e59955d041f20fc855
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-05-31 11:30:36 +0300

    dashboard employee fields to update

--- src/app/dashboard-employee/dashboard-employee.component.ts
+++ src/app/dashboard-employee/dashboard-employee.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalService } from '../login/service/local.service';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms'
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-dashboard-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './dashboard-employee.component.html',
   styleUrl: './dashboard-employee.component.css',
 })
 export class DashboardEmployeeComponent implements OnInit {
+
+  balance: number = 1000;
 
   url = "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg";
 
