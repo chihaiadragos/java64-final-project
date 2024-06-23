acbe83f04414df9f6e400acc250d9b420586c0d5
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-06-23 18:15:16 +0300

    navbar dashcomponent

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-file-upload',
@@ -8,11 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css'
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements OnInit{
   name: string = '';
   file: any;
+  customerImage: string | null = null;
 
   constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('http://localhost:8080/customer/304/image', { responseType: 'text' })
+    .subscribe(
+      data => this.customerImage = 'data:image/png;base64,' + data,
+      error => console.error(error)
+    );
+  }
 
   getName(name: string) {
     this.name = name;
@@ -28,7 +36,7 @@ export class FileUploadComponent {
     formData.set('id', this.name);
     formData.set('file', this.file);
 
-    this.http.post('http://localhost:8080/customer/updateimage/3', formData).subscribe(response => {
+    this.http.post('http://localhost:8080/customer/updateimage/12', formData).subscribe(response => {
       console.log(response);
     });
   }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,14 +6,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatTabsModule} from '@angular/material/tabs';
+import { HomeComponent } from '../routes/home/home.component';
+import { AddReservationComponent } from '../add-reservation/add-reservation.component';
+import { ReservationsComponent } from '../reservations/reservations.component';
+import { AddEmployeeComponent } from "../add-employee/add-employee.component";
+import { FileUploadComponent } from "../file-upload/file-upload.component";
+import { AddCarComponent } from "../add-car/add-car.component";
+import { AddBranchComponent } from "../add-branch/add-branch.component";
+import { BranchesComponent } from "../branches/branches.component";
 
 @Component({
-  selector: 'app-test',
-  standalone: true,
-  imports: [RouterModule, CommonModule, MatButtonModule, MatMenuModule, MatIconModule,MatGridListModule],
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+    selector: 'app-test',
+    standalone: true,
+    templateUrl: './test.component.html',
+    styleUrl: './test.component.css',
+    imports: [RouterModule, HomeComponent, AddReservationComponent, ReservationsComponent, CommonModule, MatButtonModule, MatMenuModule, MatIconModule, MatGridListModule, MatTabsModule, AddEmployeeComponent, FileUploadComponent, AddCarComponent, AddBranchComponent, BranchesComponent]
 })
 export class TestComponent {
   constructor(private router: Router, private localService: LocalService) { }
+  selectedTabIndex: number = 0;
+
+  onTabChange(event: any): void {
+    this.selectedTabIndex = event.index;
+  }
 }
