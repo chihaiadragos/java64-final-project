f83c245eb18515d43cd2b87ca067b39b01141d90
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-06-27 11:11:03 +0300

    fix

--- src/app/add-branch/add-branch.component.ts
+++ src/app/add-branch/add-branch.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalService } from '../login/service/local.service';
 import { BranchService } from '../service/branch.service';
@@ -11,23 +17,29 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-add-branch',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatSelectModule, MatFormFieldModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './add-branch.component.html',
-  styleUrl: './add-branch.component.css'
+  styleUrl: './add-branch.component.css',
 })
 export class AddBranchComponent {
-  constructor(private router: Router, private formBuilder: FormBuilder, private localService: LocalService, private branchService: BranchService){}
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private localService: LocalService,
+    private branchService: BranchService
+  ) {}
 
-  register: FormGroup = this.formBuilder.group(
-    {
-      city: ['', Validators.required],
-      address: ['', Validators.required]
-    }
-  );
+  register: FormGroup = this.formBuilder.group({
+    city: ['', Validators.required],
+    address: ['', Validators.required],
+  });
 
   public registerNow() {
-    
-
     const branchRegister = new Branch(
       -1,
       this.register.get('city')?.value,
@@ -36,28 +48,28 @@ export class AddBranchComponent {
 
     console.log(branchRegister);
 
-    this.branchService.createBranch(branchRegister).subscribe((data)=>{
+    this.branchService.createBranch(branchRegister).subscribe(
+      (data) => {
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'New branch successfully added',
+          showConfirmButton: false,
+          timer: 3500,
+        });
 
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "New branch successfully added",
-        showConfirmButton: false,
-        timer: 3500
-      });
-
-      this.register.reset();
-      this.router.navigate(['test']);
-    },
-    (error) => {
-      Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Branch already exist",
-        showConfirmButton: false,
-        timer: 6000
-      });
-    }
-  )
+        this.register.reset();
+        this.router.navigate(['test']);
+      },
+      (error) => {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Branch already exist',
+          showConfirmButton: false,
+          timer: 4000,
+        });
+      }
+    );
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { LoginService } from './service/login.service';
 import Login from '../types/login';
 import { LocalService } from './service/local.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -27,6 +28,13 @@ export class LoginComponent {
   public loginNow(){
     const userLogin = new Login(this.login.get('email')?.value, this.login.get('password')?.value);
     this.loginService.login(userLogin).subscribe((data)=>{
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Logging in!',
+        showConfirmButton: false,
+        timer: 3500,
+      });
       console.log(data);
       this.localService.saveData("currentUser", JSON.stringify({
         id: data.id,
@@ -35,9 +43,19 @@ export class LoginComponent {
         token: data.token
       }));
       this.router.navigate(['']);
+      
+    },(error) => {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Invalid request',
+        showConfirmButton: false,
+        timer: 4000,
+      });
     })
- 
   }
 
   
 }
+
+
